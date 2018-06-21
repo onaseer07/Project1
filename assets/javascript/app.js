7ac2da2e616299865be239b77c3a0bb9bf7a8b84
onaseer07 <onaseer07@gmail.com>
2018-06-20 20:35:55 -0500

    Completed UI and Integerated API pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,71 @@
 $(document).ready(function(){
 
 
-    $("body").html(`
-    <div id = "searchDiv">
-    <input type ="text" value="" id = "search">
-    <button id="submit">Check BestBuy Inventory</button>
-    `);
-
-    
 
     $("#submit").on("click", function(event){
         event.preventDefault();
+        var term = $("#search").val().trim();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api-endpoint.igdb.com/games/?search="+term+"&fields=name",
+            "method": "GET",
+            "headers": {
+              "user-key": "186a3663d438fb1004a180d435b09d81",
+              "accept": "application/json"
+            },
+          };
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+            /*$("#gameTitleHeader").append(`
+            <h1 class = "gameTitleHeader"id = ${response[0].name}>${response[0].name}</h1>
+            <h2> Check Out Similar Games</h2>
+            <p id = "similarGames">${response[1].name}</p>
+            `);*/
+            
+            var bestBuyTerm = {
+                firstResponse:response[0].name.trim(),
+            };
+            var bestBuysettings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api.bestbuy.com/v1/products%28%28search="+JSON.stringify(bestBuyTerm.firstResponse)+"%29%29?apiKey=A1KfLKNYXxCux4LzG87ur1tV&format=json",
+                "method": "GET"
+                };
+    
+            $.ajax(bestBuysettings).done(function (bestBuyData) {
+                console.log(bestBuyData);
+    
+                if (bestBuyData.products["0"].onlineAvailability = true){
+                    $("#gameTitleHeader").append(`
+                    <h6>${bestBuyData.products["0"].name} is available to buy online!
+                    `)
+                } else if (bestBuyData.products["0"].onlineAvailability = false){
+                    $("#gameTitleHeader").append(`
+                    <h6>Sorry! ${bestBuyData.products["0"].name} is out of stock.
+                    `) 
+                } else {
+                    $("#gameTitleHeader").append(`
+                    <h6>Error
+                    `)
+                }
+              }); 
+          });
 
-        let term = $("#search").val().trim();
+    })
+    //IGDB API Call Begins
 
-        let settings = {
+    //IGDB API Call Ends
+
+    //Best Buy API Call Begins
+
+    /*$("#submit").on("click", function(event){
+        event.preventDefault();
+
+        var term = $("#search").val().trim();
+
+        var settings = {
             "async": true,
             "crossDomain": true,
             "url": "https://api.bestbuy.com/v1/products%28%28search="+term+"%29%29?apiKey=A1KfLKNYXxCux4LzG87ur1tV&format=json",
@@ -25,20 +76,20 @@ $(document).ready(function(){
             console.log(response);
 
             if (response.products["0"].onlineAvailability = true){
-                $("#searchDiv").append(`
+                $("#gameTitleHeader").append(`
                 <h6>${response.products["0"].name} is available to buy online!
                 `)
             } else if (response.products["0"].onlineAvailability = false){
-                $("#searchDiv").append(`
+                $("#gameTitleHeader").append(`
                 <h6>Sorry! ${response.products["0"].name} is out of stock.
                 `) 
             } else {
-                $("#searchDiv").append(`
+                $("#gameTitleHeader").append(`
                 <h6>Error
                 `)
             }
           });
         $("#search").val("");
-    });  
-
+    });*/  
+    //Best Buy API Call Ends
 });
