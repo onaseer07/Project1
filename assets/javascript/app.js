938aae60d1bc4dbffd26eaa68c16018a77e06022
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-06-24 19:29:03 -0500

    added api calls to similar games when clicked will display relevent game info and videos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
-    $("#submit").on("click", function(event){
+    $("#submit").on("click", function (event) {
         event.preventDefault();
 
         var term = $("#search").val().trim();
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://api-endpoint.igdb.com/games/?search="+term+"$&limit=10&fields=*&order=popularity:desc&filter[rating][gte]=65",
+            "url": "https://api-endpoint.igdb.com/games/?search=" + term + "$&limit=10&fields=*&order=popularity:desc&filter[rating][gte]=65",
             "method": "GET",
             "headers": {
-              "user-key": "186a3663d438fb1004a180d435b09d81",
-              "accept": "application/json"
+                "user-key": "186a3663d438fb1004a180d435b09d81",
+                "accept": "application/json"
             },
-          };
-          
-          $.ajax(settings).done(function (response) {
+        };
+
+        $.ajax(settings).done(function (response) {
             console.log(response);
+
             $('#GameInfo').empty(); //clears divs for additional searches
             $('#Video').empty();
             $('#Similar').empty();
@@ -29,46 +30,83 @@ $(document).ready(function(){
             `);*/
             var GameInfo = $('#GameInfo')
             var Video = $('#Video')
-            var Similar = $('#Similar')
-            GameInfo.append(response[0].name +'<br>') // appends game title
+            GameInfo.append(`<div id="GameTitle">${response[0].name}</div>`) // appends game title and added ID for styling
             GameInfo.append(response[0].summary) // appends game summary
             // console.log(response[0].videos);
-            for(let i=0;i < response[0].videos.length; i++){ //loop to add all videos from object associated with game choice
-            Video.append(`<iframe frameborder=0 allowfullscreen src=https://www.youtube.com/embed/${response[0].videos[i].video_id}></iframe>`)
+            if (response[0].videos) {
+                for (var i = 0; i < response[0].videos.length; i++) { //loop to add all videos from object associated with game choice
+                    Video.append(`<iframe frameborder=0 allowfullscreen src=https://www.youtube.com/embed/${response[0].videos[i].video_id}></iframe>`)
+                }
+            } else {
+                Video.text("No Videos Available")
             }
-            
-            for(let s=1;s < response.length;s++){ //loop to show each cover thumbnail of similar games, might change to title and cover
-                Similar.append(`<img height=50px data-name="${response[s].name} "src="https:${response[s].cover.url}">`)
+            for (var s = 0; s < response.length; s++) { //loop to show each cover thumbnail of similar games, might change to title and cover
+                $('#Similar').append(`<img class="SimGameThumb" height=50px data-id="${response[s].id}" src="https:${response[s].cover.url}">`)
             }
 
+            $('.SimGameThumb').on('click', function (event) {
+                event.preventDefault();
+                var term1 = $(this).attr('data-id')
+                var settings1 = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api-endpoint.igdb.com/games/" + term1 + "?fields=*",
+                    "method": "GET",
+                    "headers": {
+                        "user-key": "186a3663d438fb1004a180d435b09d81",
+                        "accept": "application/json"
+                    },
+                };
+                $.ajax(settings1).done(function (response) {
+                    console.log(response);
+                    $('#GameInfo').empty(); //clears divs for additional searches
+                    $('#Video').empty();
+                    var GameInfo = $('#GameInfo')
+                    var Video = $('#Video')
+                    GameInfo.append(`<div id="GameTitle">${response[0].name}</div>`) // appends game title and added ID for styling
+                    GameInfo.append(response[0].summary) // appends game summary
+                    if (response[0].videos) {
+                        for (var i = 0; i < response[0].videos.length; i++) { //loop to add all videos from object associated with game choice
+                            Video.append(`<iframe frameborder=0 allowfullscreen src=https://www.youtube.com/embed/${response[0].videos[i].video_id}></iframe>`)
+                        }
+                    }
+                    else {
+                        Video.text("No Videos Available")
+                    }
+                    // for(var j=1;j < response[0].games.length;j++){
+                    //     $('#Similar').append(`<img class="SimGameThumb" height=50px data-id="${response[j].games[j].id}" src="https:${response[j].games[j].cover.url}">`)
+                    // }
+                })
+            })
+
             var bestBuyTerm = {
-                firstResponse:response[0].name.trim(),
+                firstResponse: response[0].name.trim(),
             };
             var bestBuysettings = {
                 "async": true,
                 "crossDomain": true,
-                "url": "https://api.bestbuy.com/v1/products%28%28search="+JSON.stringify(bestBuyTerm.firstResponse)+"%29%29?apiKey=A1KfLKNYXxCux4LzG87ur1tV&format=json",
+                "url": "https://api.bestbuy.com/v1/products%28%28search=" + JSON.stringify(bestBuyTerm.firstResponse) + "%29%29?apiKey=A1KfLKNYXxCux4LzG87ur1tV&format=json",
                 "method": "GET"
-                };
-    
+            };
+
             $.ajax(bestBuysettings).done(function (bestBuyData) {
                 console.log(bestBuyData);
-    
-                if (bestBuyData.products["0"].onlineAvailability = true){
+
+                if (bestBuyData.products["0"].onlineAvailability = true) {
                     $("#gameTitleHeader").append(`
                     <h6>${bestBuyData.products["0"].name} is available to buy online!
                     `)
-                } else if (bestBuyData.products["0"].onlineAvailability = false){
+                } else if (bestBuyData.products["0"].onlineAvailability = false) {
                     $("#gameTitleHeader").append(`
                     <h6>Sorry! ${bestBuyData.products["0"].name} is out of stock.
-                    `) 
+                    `)
                 } else {
                     $("#gameTitleHeader").append(`
                     <h6>Error
                     `)
                 }
-              }); 
-          });
+            });
+        });
 
     })
     //IGDB API Call Begins
@@ -107,6 +145,6 @@ $(document).ready(function(){
             }
           });
         $("#search").val("");
-    });*/  
+    });*/
     //Best Buy API Call Ends
 });
