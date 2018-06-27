e63dc53e7791e330642bdf0b2362bc15b92e1eb2
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-06-27 06:54:50 -0500

    added bottom screenshot, fixed css issues, still requires more font styling and nav bar clean up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
     $('#carouselExampleControls').carousel('pause');
-
+    $('#Similar').css('display', 'none');
+    $('#cont').css('display', 'none');
 
     $("#submit").on("click", function (event) {
         event.preventDefault();
@@ -28,21 +29,21 @@ $(document).ready(function () {
             $('#GameInfo').empty(); //clears divs for additional searches
             $('#Video').empty();
             $('#Similar').empty();
-
-
-            /*$("#gameTitleHeader").append(`
-            <h1 class = "gameTitleHeader"id = ${response[0].name}>${response[0].name}</h1>
-            <h2> Check Out Similar Games</h2>
-            <p id = "similarGames">${response[1].name}</p>
-            `);*/
+            $('#SimGamesTitle').remove();
+            $('#Similar').css('display', 'block');
+            $('#cont').css('display', 'block');
 
             var BGScreen = $(`<div style="background-image:url(https://images.igdb.com/igdb/image/upload/t_screenshot_big/${response[0].screenshots[0].cloudinary_id}.jpg);
             height:290px;width:100%;position:absolute;overflow:hidden;background-size:cover;right:0;filter:blur(5px);
             background-position-y:-160px;"></div>`).addClass('bg-image')
             $('.ScreenContainer').prepend(BGScreen)
+            var BGScreen2 = $(`<div style="background-image:url(https://images.igdb.com/igdb/image/upload/t_screenshot_big/${response[0].screenshots[0].cloudinary_id}.jpg);
+            height:290px;width:100%;position:absolute;overflow:hidden;background-size:cover;right:0;filter:blur(5px);
+            background-position-y:-560px;top: 865px;"></div>`).addClass('bg-image')
+            $('.ScreenContainer').append(BGScreen2)
             var CoverImage = $(`<img id="MainCover" src="https://images.igdb.com/igdb/image/upload/t_cover_big/${response[0].cover.cloudinary_id}.jpg" style="height=260px;width=240px;">`)
             $('.ScreenContainer').append(CoverImage)
-
+            $('#SimilarGames').prepend(`<div id="SimGamesTitle">Similar Games</div>`)
             var TwitchSettings = {
                 "async": true,
                 "crossDomain": true,
@@ -55,8 +56,13 @@ $(document).ready(function () {
             }
             $.ajax(TwitchSettings).done(function (response) {
                 console.log(response);
-
-                $('#Twitch').append(`<iframe height=300px width=100% src="http://player.twitch.tv/?channel=${response.streams[0].channel.name}&autoplay=false" allowfullscreen frameborder=0><iframe>`)
+                if (response.streams.length !== 0) {
+                    $('#Twitch').prepend(`<div id="LSTitle">Live Stream</div>`)
+                    $('#Twitch').append(`<iframe id="LS" height=300px width=100% src="http://player.twitch.tv/?channel=${response.streams[0].channel.name}&autoplay=false" allowfullscreen frameborder=0><iframe>`)
+                    $('#SimilarGames').css('left', '215px');
+                } else {
+                    $('#SimilarGames').css('left', '215px');
+                }
 
             })
 
@@ -64,8 +70,8 @@ $(document).ready(function () {
             var Video = $('#Video')
             var ScreenShots = $('#ScreenShots')
             GameInfo.append(`<div id="GameTitle">${response[0].name}</div>`) // appends game title and added ID for styling
-            GameInfo.append(response[0].summary) // appends game summary
-
+            $('#GameTitle').append(`<div id="ReleaseDate">${response[0].release_dates[0].human}</div>`);
+            GameInfo.append(`<p id="Summary">${response[0].summary}</p>`) // appends game summary
 
             if (response[0].videos) {
                 Video.append(`<div class="carousel-item active">
@@ -87,105 +93,136 @@ $(document).ready(function () {
                 }
             }
 
-            $('#Similar').append(`<div class="carousel-item active">
-            <img class="SimGameThumb d-block w-100" height="160" width="240" data-id="${response[0].id}" src="https://images.igdb.com/igdb/image/upload/t_cover_med/${response[0].cover.cloudinary_id}.jpg">
+            var SimilarGamesIdList = [];
+
+            for (var i = 0; i < response[0].games.length; i++) {
+                SimilarGamesIdList.push(response[0].games[i]);
+            }
+            var NewSimilar = SimilarGamesIdList.toString();
+
+            var settings2 = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://cors-anywhere.herokuapp.com/https://api-endpoint.igdb.com/games/" + NewSimilar + "?fields=*",
+                "method": "GET",
+                "headers": {
+                    "user-key": "186a3663d438fb1004a180d435b09d81",
+                    "accept": "application/json"
+                },
+            };
+            $.ajax(settings2).done(function (response) {
+                console.log(response);
+                $('#Similar').append(`<div class="carousel-item active">
+            <img class="SimGameThumb d-block w-100" height="300" width="240" data-id="${response[0].id}" src="https://images.igdb.com/igdb/image/upload/t_cover_med/${response[0].cover.cloudinary_id}.jpg">
             <div class="carousel-caption d-none d-sm-block">
             <p id="CText">${response[0].name}</p>
             </div>
             </div>`)
-            for (var s = 1; s < response.length; s++) { //loop to show each cover thumbnail of similar games, might change to title and cover
-                if (response[s].cover) {
-                    $('#Similar').append(`<div class="carousel-item">
-                    <img class="SimGameThumb d-block w-100" height="160" width="240" data-id="${response[s].id}" src="https://images.igdb.com/igdb/image/upload/t_cover_big/${response[s].cover.cloudinary_id}.jpg">
+                for (var s = 1; s < response.length; s++) { //loop to show each cover thumbnail of similar games, might change to title and cover
+                    if (response[s].cover) {
+                        $('#Similar').append(`<div class="carousel-item">
+                    <img class="SimGameThumb d-block w-100" height="300" width="240" data-id="${response[s].id}" src="https://images.igdb.com/igdb/image/upload/t_cover_big/${response[s].cover.cloudinary_id}.jpg">
                     <div class="carousel-caption d-none d-sm-block">
                     <p id="CText">${response[s].name}</p>
                     </div>
                     </div>`)
+                    }
                 }
-            }
-            $('.SimGameThumb').on('click', function (event) { //click function to change main display when a similar game is clicked on
-                event.preventDefault();
+                $('.SimGameThumb').on('click', function (event) { //click function to change main display when a similar game is clicked on
+                    event.preventDefault();
 
-                var term1 = $(this).attr('data-id')
-                var settings1 = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://cors-anywhere.herokuapp.com/https://api-endpoint.igdb.com/games/" + term1 + "?fields=*",
-                    "method": "GET",
-                    "headers": {
-                        "user-key": "186a3663d438fb1004a180d435b09d81",
-                        "accept": "application/json"
-                    },
-                };
-                $.ajax(settings1).done(function (response) {
-                    console.log(response);
-                    $('.ScreenContainer').empty();
-                    $('#Twitch').empty();
-                    $('#GameInfo').empty(); //clears divs for additional searches
-                    $('#Video').empty();
-                    var BGScreen = $(`<div style="background-image:url(https://images.igdb.com/igdb/image/upload/t_screenshot_big/${response[0].screenshots[0].cloudinary_id}.jpg);
-                    height:290px;width:100%;position:absolute;overflow:hidden;background-size:cover;right:0;filter:blur(5px);
-                    background-position-y:-160px;"></div>`).addClass('bg-image')
-                    $('.ScreenContainer').prepend(BGScreen)
-                    var CoverImage = $(`<img id="MainCover" src="https://images.igdb.com/igdb/image/upload/t_cover_big/${response[0].cover.cloudinary_id}.jpg" style="height=260px;width=240px;">`)
-                    $('.ScreenContainer').append(CoverImage)
-                    var GameInfo = $('#GameInfo')
-                    var Video = $('#Video')
-                    GameInfo.append(`<div id="GameTitle">${response[0].name}</div>`) // appends game title and added ID for styling
-                    GameInfo.append(response[0].summary) // appends game summary
-                    var TwitchSettings = {
+                    var term1 = $(this).attr('data-id')
+                    var settings1 = {
                         "async": true,
                         "crossDomain": true,
-                        "url": "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/streams/?game=" + response[0].name + "&limit=5",
+                        "url": "https://cors-anywhere.herokuapp.com/https://api-endpoint.igdb.com/games/" + term1 + "?fields=*",
                         "method": "GET",
                         "headers": {
-                            "Accept": "application/vnd.twitchtv.v5+json",
-                            "Client-ID": "miw3hauybsi442h7ysse8i4y46m3ny",
-                        }
-                    }
-                    $.ajax(TwitchSettings).done(function (response) {
+                            "user-key": "186a3663d438fb1004a180d435b09d81",
+                            "accept": "application/json"
+                        },
+                    };
+                    $.ajax(settings1).done(function (response) {
                         console.log(response);
+                        $('.ScreenContainer').empty();
+                        $('#Twitch').empty();
+                        $('#GameInfo').empty(); //clears divs for additional searches
+                        $('#Video').empty();
+                        var BGScreen = $(`<div style="background-image:url(https://images.igdb.com/igdb/image/upload/t_screenshot_big/${response[0].screenshots[0].cloudinary_id}.jpg);
+                    height:290px;width:100%;position:absolute;overflow:hidden;background-size:cover;right:0;filter:blur(5px);
+                    background-position-y:-160px;"></div>`).addClass('bg-image')
+                        $('.ScreenContainer').prepend(BGScreen)
+                        var BGScreen2 = $(`<div style="background-image:url(https://images.igdb.com/igdb/image/upload/t_screenshot_big/${response[0].screenshots[0].cloudinary_id}.jpg);
+                        height:290px;width:100%;position:absolute;overflow:hidden;background-size:cover;right:0;filter:blur(5px);
+                        background-position-y:-560px;top: 865px;"></div>`).addClass('bg-image')
+                        $('.ScreenContainer').append(BGScreen2)
+                        var CoverImage = $(`<img id="MainCover" src="https://images.igdb.com/igdb/image/upload/t_cover_big/${response[0].cover.cloudinary_id}.jpg" style="height=260px;width=240px;">`)
+                        $('.ScreenContainer').append(CoverImage)
+                        var GameInfo = $('#GameInfo')
+                        var Video = $('#Video')
+                        GameInfo.append(`<div id="GameTitle">${response[0].name}</div>`) // appends game title and added ID for styling
+                        $('#GameTitle').append(`<div id="ReleaseDate">${response[0].release_dates[0].human}</div>`);
+                        GameInfo.append(`<p id="Summary">${response[0].summary}</p>`) // appends game summary
+                        var TwitchSettings = {
+                            "async": true,
+                            "crossDomain": true,
+                            "url": "https://cors-anywhere.herokuapp.com/https://api.twitch.tv/kraken/streams/?game=" + response[0].name + "&limit=5",
+                            "method": "GET",
+                            "headers": {
+                                "Accept": "application/vnd.twitchtv.v5+json",
+                                "Client-ID": "miw3hauybsi442h7ysse8i4y46m3ny",
+                            }
+                        }
+                        $.ajax(TwitchSettings).done(function (response) {
+                            if (response.streams.length !== 0) {
+                                $('#Twitch').prepend(`<div id="LSTitle">Live Stream</div>`)
+                                $('#Twitch').append(`<iframe id="LS" height=300px width=100% src="http://player.twitch.tv/?channel=${response.streams[0].channel.name}&autoplay=false" allowfullscreen frameborder=0><iframe>`)
+                                $('#SimilarGames').css('left', '215px');
+                            } else {
+                                $('#SimilarGames').css('left', '215px');
+                            }
 
-                        $('#Twitch').append(`<iframe height=300px width=100% src="http://player.twitch.tv/?channel=${response.streams[0].channel.name}&autoplay=false" allowfullscreen frameborder=0><iframe>`)
-
-                    })
-                    if (response[0].videos) {
-                        Video.append(`<div class="carousel-item active">
+                        })
+                        if (response[0].videos) {
+                            Video.append(`<div class="carousel-item active">
                         <iframe class="d-block w-100" frameborder=0 height=300px width=300px allowfullscreen src="https://www.youtube.com/embed/${response[0].videos[0].video_id}"></iframe>
                         </div>`)
 
-                        for (var i = 1; i < response[0].videos.length; i++) { //loop to add all videos from object associated with game choice
-                            Video.append(`<div class="carousel-item">
+                            for (var i = 1; i < response[0].videos.length; i++) { //loop to add all videos from object associated with game choice
+                                Video.append(`<div class="carousel-item">
                             <iframe class="d-block w-100" frameborder=0 height=300px width=300px allowfullscreen src="https://www.youtube.com/embed/${response[0].videos[i].video_id}"></iframe>
                             </div>`)
-                        }
-                    } else {
-                        Video.empty();
-                    }
-                    if (response[0].screenshots) {
-                        if (!response[0].videos) {
-                            Video.append(`<div class="carousel-item active">
-                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[0].cloudinary_id}.jpg"></div>`)
-
-                            for (var i = 1; i < response[0].screenshots.length; i++) {
-                                Video.append(`<div class="carousel-item">
-                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[i].cloudinary_id}.jpg"></div>`)
                             }
                         } else {
-                            for (var i = 0; i < response[0].screenshots.length; i++) {
-                                Video.append(`<div class="carousel-item">
-                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[i].cloudinary_id}.jpg"></div>`)
-                            }
+                            Video.empty();
                         }
+                        if (response[0].screenshots) {
+                            if (!response[0].videos) {
+                                Video.append(`<div class="carousel-item active">
+                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[0].cloudinary_id}.jpg"></div>`)
 
-                    }
+                                for (var i = 1; i < response[0].screenshots.length; i++) {
+                                    Video.append(`<div class="carousel-item">
+                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[i].cloudinary_id}.jpg"></div>`)
+                                }
+                            } else {
+                                for (var i = 0; i < response[0].screenshots.length; i++) {
+                                    Video.append(`<div class="carousel-item">
+                <img class="d-block w-100" height="300" width="300" src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/${response[0].screenshots[i].cloudinary_id}.jpg"></div>`)
+                                }
+                            }
+
+                        }
+                    })
                 })
+
+
             })
 
             var bestBuyTerm = {
                 firstResponse: response[0].name.trim(),
 
-            
+
             };
             var bestBuysettings = {
                 "async": true,
